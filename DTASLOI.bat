8d172863979274a8c1d69046ae679f844ac3d8cf
T Line <124744173+TLine797@users.noreply.github.com>
2026-03-21 20:55:57 -0400

    Update process termination method and clean up script

--- DTASLOI.bat
+++ DTASLOI.bat
@@ -1,5 +1,6 @@
 @echo off
 set "PROCESS=Gorilla Tag.exe"
+set "PROCESSNAME=Gorilla Tag"
 set "POSSIBLELOC1=C:\Program Files (x86)\Steam\steamapps\common\Gorilla Tag\BepInEx\plugins"
 set "POSSIBLELOC2=C:\Program Files\Oculus\Software\Software\another-axiom-gorilla-tag\BepInEx\plugins"
 set "POSSIBLELOC3=C:\Program Files\Meta Horizon\Software\Software\another-axiom-gorilla-tag\BepInEx\plugins"
@@ -8,7 +9,7 @@ set "COMPANY=DangThatsAShitLoadOfInfo"
 :check
 tasklist | find /I "%PROCESS%" >nul 2>&1
 if %errorlevel%==0 (
-    taskkill /f /im "%PROCESS%" 2>nul
+    powershell -NoProfile -Command "Stop-Process -Name '%PROCESSNAME%' -Force -ErrorAction SilentlyContinue"
     timeout /t 2 >nul
     goto check
 )
